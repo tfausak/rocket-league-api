b66f2b5a58c6a72a8c02c711dc3d4d9cf6ba0cc7
Taylor Fausak <taylor@fausak.me>
2017-05-07 09:13:11 -0500

    Define types for inputs

--- executables/rocket-league-api.hs
+++ executables/rocket-league-api.hs
@@ -1,10 +1,10 @@
+{-# LANGUAGE OverloadedLists #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 module Main where
 
 import Control.Monad
 import Control.Monad.IO.Class
-import Data.Aeson
 import Data.Text
 import Network.HTTP.Client.TLS
 import RocketLeagueApi
@@ -19,16 +19,19 @@ main = do
     token = Just . Token $ pack rawToken
     baseUrl = BaseUrl Https "api.rocketleaguegame.com" 443 "/api/v1"
     clientEnv = ClientEnv manager baseUrl
-    platform = "steam"
-    playerId = "76561198139217900"
+    platform = PlatformSteam
+    playerId = PlayerIdSteam 76561198139217900
+    playerIds = PlayerIds [playerId]
+    playlist = PlaylistCompetitiveSoloDuel
+    statType = StatTypeGoals
   void . flip runClientM clientEnv $ do
     liftIO . print =<< getPlayerSkills platform playerId token
-    liftIO . print =<< postPlayerSkills platform (object ["player_ids" .= [playerId]]) token
+    liftIO . print =<< postPlayerSkills platform playerIds token
     liftIO . print =<< getPlayerTitles platform playerId token
     liftIO . print =<< getPopulation token
     liftIO . print =<< getRegions token
-    liftIO . print =<< getSkillsLeaderboard platform "10" token
+    liftIO . print =<< getSkillsLeaderboard platform playlist token
     liftIO . print =<< getStatsLeaderboard platform token
-    liftIO . print =<< getStatLeaderboard platform "goals" token
-    liftIO . print =<< getPlayerStat platform "goals" playerId token
-    liftIO . print =<< postPlayerStat platform "goals" (object ["player_ids" .= [playerId]]) token
+    liftIO . print =<< getStatLeaderboard platform statType token
+    liftIO . print =<< getPlayerStat platform statType playerId token
+    liftIO . print =<< postPlayerStat platform statType playerIds token
--- library/RocketLeagueApi.hs
+++ library/RocketLeagueApi.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TypeOperators #-}
 
 -- https://api.rocketleaguegame.com/docs/
@@ -7,20 +8,21 @@ module RocketLeagueApi where
 import Data.Aeson
 import Data.Monoid
 import Data.Proxy
+import Data.Set
 import Data.Text
 import Servant.API
 import Servant.Client
 
-getPlayerSkills :: Text -> Text -> Maybe Token -> ClientM Value
-postPlayerSkills :: Text -> Value -> Maybe Token -> ClientM Value
-getPlayerTitles :: Text -> Text -> Maybe Token -> ClientM Value
+getPlayerSkills :: Platform -> PlayerId -> Maybe Token -> ClientM Value
+postPlayerSkills :: Platform -> PlayerIds -> Maybe Token -> ClientM Value
+getPlayerTitles :: Platform -> PlayerId -> Maybe Token -> ClientM Value
 getPopulation :: Maybe Token -> ClientM Value
 getRegions :: Maybe Token -> ClientM Value
-getSkillsLeaderboard :: Text -> Text -> Maybe Token -> ClientM Value
-getStatsLeaderboard :: Text -> Maybe Token -> ClientM Value
-getStatLeaderboard :: Text -> Text -> Maybe Token -> ClientM Value
-getPlayerStat :: Text -> Text -> Text -> Maybe Token -> ClientM Value
-postPlayerStat :: Text -> Text -> Value -> Maybe Token -> ClientM Value
+getSkillsLeaderboard :: Platform -> Playlist -> Maybe Token -> ClientM Value
+getStatsLeaderboard :: Platform -> Maybe Token -> ClientM Value
+getStatLeaderboard :: Platform -> StatType -> Maybe Token -> ClientM Value
+getPlayerStat :: Platform -> StatType -> PlayerId -> Maybe Token -> ClientM Value
+postPlayerStat :: Platform -> StatType -> PlayerIds -> Maybe Token -> ClientM Value
 
 getPlayerSkills
   :<|> postPlayerSkills
@@ -38,23 +40,100 @@ api :: Proxy Api
 api = Proxy
 
 type Api
-  = Capture "platform" Text :> "playerskills" :> Capture "player_id" Text :> Endpoint Get Value
-  :<|> Capture "platform" Text :> "playerskills" :> ReqBody '[JSON] Value :> Endpoint Post Value
-  :<|> Capture "platform" Text :> "playertitles" :> Capture "player_id" Text :> Endpoint Get Value
+  = CapturePlatform :> "playerskills" :> CapturePlayerId :> Endpoint Get Value
+  :<|> CapturePlatform :> "playerskills" :> BodyPlayerIds :> Endpoint Post Value
+  :<|> CapturePlatform :> "playertitles" :> CapturePlayerId :> Endpoint Get Value
   :<|> "population" :> Endpoint Get Value
   :<|> "regions" :> Endpoint Get Value
-  :<|> Capture "platform" Text :> "leaderboard" :> "skills" :> Capture "playlist" Text :> Endpoint Get Value
-  :<|> Capture "platform" Text :> "leaderboard" :> "stats" :> Endpoint Get Value
-  :<|> Capture "platform" Text :> "leaderboard" :> "stats" :> Capture "stat_type" Text :> Endpoint Get Value
-  :<|> Capture "platform" Text :> "leaderboard" :> "stats" :> Capture "stat_type" Text :> Capture "player_id" Text :> Endpoint Get Value
-  :<|> Capture "platform" Text :> "leaderboard" :> "stats" :> Capture "stat_type" Text :> ReqBody '[JSON] Value :> Endpoint Post Value
+  :<|> CapturePlatform :> "leaderboard" :> "skills" :> CapturePlaylist :> Endpoint Get Value
+  :<|> CapturePlatform :> "leaderboard" :> "stats" :> Endpoint Get Value
+  :<|> CapturePlatform :> "leaderboard" :> "stats" :> CaptureStatType :> Endpoint Get Value
+  :<|> CapturePlatform :> "leaderboard" :> "stats" :> CaptureStatType :> CapturePlayerId :> Endpoint Get Value
+  :<|> CapturePlatform :> "leaderboard" :> "stats" :> CaptureStatType :> BodyPlayerIds :> Endpoint Post Value
 
 type Endpoint method result
   = Header "Authorization" Token :> method '[JSON] result
 
+type CapturePlatform = Capture "platform" Platform
+type CapturePlayerId = Capture "player_id" PlayerId
+type BodyPlayerIds = ReqBody '[JSON] PlayerIds
+type CapturePlaylist = Capture "playlist" Playlist
+type CaptureStatType = Capture "stat_type" StatType
+
 newtype Token = Token
   { tokenValue :: Text
-  } deriving (Eq, Show)
+  } deriving (Eq, Ord, Show)
 
 instance ToHttpApiData Token where
   toUrlPiece token = pack "Token " <> tokenValue token
+
+data Platform
+  = PlatformPS4
+  | PlatformSteam
+  | PlatformXboxOne
+  deriving (Eq, Ord, Show)
+
+instance ToHttpApiData Platform where
+  toUrlPiece platform = case platform of
+    PlatformPS4 -> "ps4"
+    PlatformSteam -> "steam"
+    PlatformXboxOne -> "xboxone"
+
+data PlayerId
+  = PlayerIdPS4 Text
+  | PlayerIdSteam Integer
+  | PlayerIdXboxOne Text
+  deriving (Eq, Ord, Show)
+
+instance ToHttpApiData PlayerId where
+  toUrlPiece playerId = case playerId of
+    PlayerIdPS4 x -> toUrlPiece x
+    PlayerIdSteam x -> toUrlPiece x
+    PlayerIdXboxOne x -> toUrlPiece x
+
+instance ToJSON PlayerId where
+  toJSON playerId = case playerId of
+    PlayerIdPS4 x -> toJSON x
+    PlayerIdSteam x -> toJSON x
+    PlayerIdXboxOne x -> toJSON x
+
+newtype PlayerIds = PlayerIds
+  { playerIdsValue :: Set PlayerId
+  } deriving (Eq, Ord, Show)
+
+instance ToJSON PlayerIds where
+  toJSON playerIds = object
+    [ "player_ids" .= playerIdsValue playerIds
+    ]
+
+data Playlist
+  = PlaylistCompetitiveSoloDuel
+  | PlaylistCompetitiveDoubles
+  | PlaylistCompetitiveSoloStandard
+  | PlaylistCompetitiveStandard
+  deriving (Eq, Ord, Show)
+
+instance ToHttpApiData Playlist where
+  toUrlPiece playlist = case playlist of
+    PlaylistCompetitiveSoloDuel -> "10"
+    PlaylistCompetitiveDoubles -> "11"
+    PlaylistCompetitiveSoloStandard -> "12"
+    PlaylistCompetitiveStandard -> "13"
+
+data StatType
+  = StatTypeAssists
+  | StatTypeGoals
+  | StatTypeMvps
+  | StatTypeSaves
+  | StatTypeShots
+  | StatTypeWins
+  deriving (Eq, Ord, Show)
+
+instance ToHttpApiData StatType where
+  toUrlPiece statType = case statType of
+    StatTypeAssists -> "assists"
+    StatTypeGoals -> "goals"
+    StatTypeMvps -> "mvps"
+    StatTypeSaves -> "saves"
+    StatTypeShots -> "shots"
+    StatTypeWins -> "wins"
